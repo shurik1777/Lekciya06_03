956fc6dee517682b4f5dd34b1fd24e5a927ef470
Aleksandr_Wosmerik <shurik1777@gmail.com>
2023-03-06 17:34:57 +0300

    lekciya

---
+++ 1/Program.cs
@@ -0,0 +1,190 @@
+﻿/*
+string NumberFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i}";
+    }
+    return result;
+}
+string NumberRec(int a, int b)
+{
+    if (a <= b) return $"{a}" + NumberRec(a + 1, b);
+    else return String.Empty;
+}
+Console.WriteLine(NumberFor(1,10));
+Console.WriteLine(NumberRec(1,10));
+*/
+/*
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; result += 1) ;
+    return result;
+}
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(10));
+Console.WriteLine(SumRec(10));
+*/
+/*
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+int PowerRec(int a, int n)
+{
+    //return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+    if (n == 0) return 1;
+    else return PowerRec(a, n - 1) * a;
+}
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowerFor(2, 10));
+Console.WriteLine(PowerRec(2, 10));
+Console.WriteLine(PowerRecMath(2, 10));
+*/
+/*
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++}\t{new String(word)}"); return;
+    }
+for (int i = 0; i < alphabet.Length; i++)
+{
+    word[length] = alphabet[i];
+    FindWords(alphabet, word, length + 1);
+}
+}
+FindWords("фисв", new char[2]);
+*/
+// Как посмотреть содержимое и время создание папки?
+/*
+string path = "c:/Users/Desktop/Desktop/Ucheba/HelloWorld/Examples/Examples001_HelloConsole/";
+DirectoryInfo di= new DirectoryInfo(path);
+Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+for (int i = 0; i < fi.Length; i++)
+{
+    Console.WriteLine(fi[i].Name);
+}
+*/
+// Просмотр содержимого всех папок от указанного пути
+/*
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent +" ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+
+    }
+}
+string path = @"c:/Users/Desktop/Desktop/Ucheba/HelloWorld/Examples/Examples001_HelloConsole/";
+CatalogInfo(path);
+*/
+// Рекурсия с пирамидками
+/*
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+Towers();
+*/
+// Обход разных структур
+/*
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+//                 0    1    2     3    4    5    6    7    8    9    10   11
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+
+    }
+}
+InOrderTraversal();
+*/
+// Ошибки : OutMemory StackOverflow Slowly
+/*
+decimal fRec = 0;
+decimal fIte = 0;
+
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
+
+decimal FibonacciIteration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+Console.ReadLine();
+
+//DateTime dt = DateTime.Now;
+
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte.ToString("### ### ###"), -15}");
+    fIte = 0;
+}
+//Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+Console.WriteLine();
+Console.ReadLine();
+//dt = DateTime.Now;
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fIte = {fRec.ToString("### ### ###"), -15}");
+    fRec = 0; 
+}
+Console.ReadLine();
+//Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+*/
+// Stack overflow
+/*
+void Rec()
+    {
+        Rec();
+    }
+Rec();
+*/
